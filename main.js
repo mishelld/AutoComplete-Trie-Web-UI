008fb9298d5bbfee9cc732e554657991a173c2b6
mishelld <mishelldub@gmail.com>
2026-01-28 16:11:29 +0200

    feat: Add word count display, input validation, and user feedback messages with associated styling.

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 import AutoCompleteTrie from "./model.js";
 
 const container = document.getElementById("container");
+const counter = document.querySelector("#counter");
+const message = document.querySelector("#message");
 const button = document.getElementById("button");
 const trie = new AutoCompleteTrie();
 
 button.addEventListener("click", () => {
   const word = document.getElementById("word");
+  if (word.value === "") {
+    message.innerHTML = "✘ Cannot add empty word";
+    message.classList.remove("green");
+    message.classList.add("red");
+
+    return;
+  }
   trie.addWord(word.value);
-  console.log(trie.words);
+  message.classList.remove("red");
+  message.classList.add("green");
+  message.innerHTML = `✓ Added '${word.value}' to dictionary `;
+  counter.innerHTML = trie.words + "";
 });
 
 const suggestion = document.getElementById("suggestion");
